8e6f0c8bcfa22a7de0c32e95c3ea6f01a1805c87
higorpa <96433962+higorpa@users.noreply.github.com>
2021-12-20 12:04:11 -0400

    commit final

---
+++ script.js
@@ -0,0 +1,30 @@
+/* codigo dark-mode*/
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function() {
+  $html.classList.toggle('dark-mode')
+  let dark_logo = document.getElementById("logodark");
+  // alert(dark_logo.src);
+  dark_logo.src = "./imagens/logo_dark.jpeg"
+  }
+)
+
+/*codigo acessibilidade*/ 
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
